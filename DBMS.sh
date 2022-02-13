bf609b740bc2435ee6ceb660f287b5465ec4ba84
“your <rofida@localhost.localdomain>
2022-02-13 07:28:19 +0200

    create&delete

--- DBMS.sh
+++ DBMS.sh
@@ -1,61 +1,99 @@
-#!/bin/bash
-####################Table_Part#########################
+#!/usr/bin/bash
+shopt -s extglob
+export LC_COLLATE=C
+mkdir -p ./Databases
+echo "Welcome To Table-Part"
 
-function tableMenu {
-        echo -e "\n______________TABLE_MENU_________________"
-        echo "|__1.Create New Table__|"
-        echo "|__2.List Extisting Tables__|"
-        echo "|__3.Insert Into Tables__|"
-        echo "|__4.Select From Tables__|"
-        echo "|__5.Delect From Tables__|"
-        echo "|__6.Drop Table__|"
-        echo "|__7.Back To Main Menu__|"
-        echo "|__8.Exit__|"
-        echo "+-------------------------------+"
-        echo -e "Enter Choice: \c"         
-read ch
-  case $ch in
-    1)  createTable ;;
-    2)  ls .; tableMenu ;;
-    3)  insert ;;
-    4)  clear; selectMenu ;;
-    5)  deleteFromTable;;
-    6)  dropTable;;
-    7) clear; cd ../.. 2>>./.error.log; mainMenu ;;
-    8) exit ;;
-    *) echo " Wrong Choice " ; tableMenu;
-  esac
+function tableMenue {
+    select choice in "press 1 to Create table" "press 2 to list Extisitng Table " "press 3 to insert into table" "press 4 to select from table" "press 5 to delete from table " "press 6 to drop table" "press 7 to exit"
+    do 
+        case $REPLY in 
+        1)create new table
+            ;;
 
+	2)list Extisitng Table 
+		;;
+        3)insert into table
+                ;;
+            
+        4)selectfrom table
+                ;;    
+	5)delete from table
+		;;
+	6)drop table
+		;;
+	7)exit
+		;;
+	
+*) echo $REPLY is not one of the choices.
+	;;
+	esac 
+done 
 }
 
-####________________Create New Table________________####
 function createTable {
-read -p "Enter Table Name: " tbname
-if [[ -f data/$1/$tbname ]]; then
-	echo "Table $tbname already exists";
-else
-	touch data/$1/$tbname;
-	read -p "Enter No of columns :" n;
+    echo "Please,Enter Table Name"
+    read tableName
+    echo "Enter number of columns"
+    read colNum
+    testInput $colNum "int"
+    if [ $? -eq 0 ]
+    then
+    echo "Please enter number of columns in integer type"
+    tableMenue
+    fi
+     if [ -f $tableName ]
+    then
+        echo "Table already exist"
+        tableMenue
+    else
+        touch $tableName
+    fi
+    typeset -i i=0
+    columns=""
+    while [ $i -lt $colNum ] 
+    do
+        echo "Enter column Name"
+        read colName
+        echo "Select column type"
+        select type in "int" "string"
+        do
+            colType=$type
+            break
+        done
+        # read colType
+        if [ -z $pk ]
+        then
+            echo "Primarykey??"
+            select answer in "yes" "no"
+            do 
+                case $REPLY in 
+                1) 
+                    pk=$colName
+                    colName+="(pk)"
+                    break;;
+                2)  break;;
+                *) echo " Please Select from yes or no " ;;
+                esac
+            done
+        fi
+        columns+="$colName:$colType;"
+        i=$i+1
+    done
+    printf $columns>>$tableName;
+    # echo pk:$pk>>$tableName;
+    tableMenue
+}    
 
-	for (( i = 1; i <= n; i++ )); do
-		read -p "Enter column $i name :" name;
-		read -p "Enter column datatype : [string/int]" dtype;
-		while [[ "$dtype" != *(int)*(string) || -z "$dtype" ]]; do
-			echo "Invalid datatype";
-			read -p "Enter column datatype : [string/int]" dtype;
-		done
-		if [[ i -eq n ]]; then
-			echo  $name >> data/$1/$tbname;
-			echo  $dtype >> data/$1/$tbname.tp;
-
-		else
-			echo -n $name":" >> data/$1/$tbname;
-			echo -n $dtype":" >> data/$1/$tbname.tp;
-		fi
-
-	done
-	echo "$tbname has been created"
-fi
+function dropTable {
+  echo -e "Enter Table Name: \c"
+  read tName
+  rm $tName .$tName 2>>./.error.log
+  if [[ $? == 0 ]]
+  then
+    echo "Table Dropped Successfully"
+  else
+    echo "Error Dropping Table $tName"
+  fi
+  tablesMenu
 }
-
-
